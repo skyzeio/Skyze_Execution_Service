6cb8e90983942020dab2c29553e194b3327a8b3d
mikenew <mikenew16@gmail.com>
2018-01-11 15:26:48 +1100

    renaming

---
+++ settings_skyze.py
@@ -0,0 +1,8 @@
+"""Skzye Market Data Updater Service Settings"""
+
+# PATHS
+data_file_path = "../Data/Trading/"
+log_file_path = "Logs/"
+test_data_file_path = "Unit_Test/Test_Data/"
+target_results_file_path = "Unit_Test/Test_Data/"
+test_results_file_path = "Unit_Test/Test_Results/"
