6c187647430c584ca6ec15a17aec429dd4a93f5a
mikenew <mikenew16@gmail.com>
2018-01-12 16:41:33 +1100

    New super class init

--- Skyze_Messaging_Service/Messages/MessageDataReceived.py
+++ Skyze_Messaging_Service/Messages/MessageDataReceived.py
@@ -8,32 +8,29 @@ from Skyze_Messaging_Service.Messages.MessageSkyzeAbstract import *
 
 
 class MessageDataReceived(MessageSkyzeAbstract):
-    """Data has been fully loaded and ready for use"""
+  """Data has been fully loaded and ready for use"""
 
-    def __init__(self, exchange, market_pair, interval):
-        """Constructor"""
-        super().__init__(SkyzeMessageType.NEW_MARKET_DATA)
-        self.__exchange = exchange
-        self.__market_pair = market_pair
-        self.__interval = interval
-        self.__message_content = f"{exchange}:{market_pair}:{interval}"
+  def __init__(self, exchange, market_pair, interval):
+    """Constructor"""
+    message_content = f"{exchange}:{market_pair}:{interval}"
+    super().__init__(SkyzeMessageType.NEW_MARKET_DATA,
+                     message_content)
+    self.__exchange = exchange
+    self.__market_pair = market_pair
+    self.__interval = interval
 
-    def getMarketPair(self):
-        """Getter"""
-        return self.__market_pair
+  def getMarketPair(self):
+    """Getter"""
+    return self.__market_pair
 
-    def getExchange(self):
-        """Getter"""
-        return self.__exchange
+  def getExchange(self):
+    """Getter"""
+    return self.__exchange
 
-    def getInterval(self):
-        """Getter"""
-        return self.__interval
+  def getInterval(self):
+    """Getter"""
+    return self.__interval
 
-    def getMessageContent(self):
-        """Getter"""
-        return self.__message_content
-
-    def getJSON(self):
-        """Return object as JSON"""
-        return super().getJSON() + "}"  # json.dumps(self.__dict__)
+  def getJSON(self):
+    """Return object as JSON"""
+    return super().getJSON() + "}"  # json.dumps(self.__dict__)
--- Skyze_Messaging_Service/Messages/MessageMarketDataUpdaterRunAll.py
+++ Skyze_Messaging_Service/Messages/MessageMarketDataUpdaterRunAll.py
@@ -8,22 +8,19 @@ from Skyze_Messaging_Service.Messages.MessageSkyzeAbstract import *
 
 
 class MessageMarketDataUpdaterRunAll(MessageSkyzeAbstract):
-    """Data has been fully loaded and ready for use"""
+  """Data has been fully loaded and ready for use"""
 
-    def __init__(self, exchange):
-        """Constructor"""
-        super().__init__(SkyzeMessageType.MARKET_DATA_UPDATER_RUN_ALL)
-        self.__exchange = exchange
-        self.__message_content = f"{exchange}:ALL PAIRS AND INTERVALS"
+  def __init__(self, exchange):
+    """Constructor"""
+    message_content = f"{exchange}:ALL PAIRS AND INTERVALS"
+    super().__init__(SkyzeMessageType.MARKET_DATA_UPDATER_RUN_ALL,
+                     message_content)
+    self.__exchange = exchange
 
-    def getExchange(self):
-        """Getter"""
-        return self.__exchange
+  def getExchange(self):
+    """Getter"""
+    return self.__exchange
 
-    def getMessageContent(self):
-        """Getter"""
-        return self.__message_content
-
-    def getJSON(self):
-        """Return object as JSON"""
-        return super().getJSON() + "}" 
+  def getJSON(self):
+    """Return object as JSON"""
+    return super().getJSON() + "}"
